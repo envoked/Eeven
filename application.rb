7d1baf8ed0940a3dc4207f7d58bb5175bd154170
Adrian Wisernig <adrian.wisernig@envoked.com>
2011-03-30 23:37:09 -0400

    Refactored a bit of code

--- application.rb
+++ application.rb
@@ -51,8 +51,7 @@ class Eeven < Sinatra::Base
 
   get '/split/isActive/:id' do
     key = "eeven:#{params[:id]}"
-    # being clever-- maybe it's slower then the corresponding conditonal      
-    active = !([nil,request.ip].include?(MEMCACHE.get(key)))
+    active = MEMCACHE.get(key) != request.ip
     MEMCACHE.set(key, request.ip, 60)
     return active.to_s
 
